1ff7685f64b68ac08c2a9f1e0195dabb1173512a
Duncan Walker <walkerdu@bc.edu>
2014-06-25 18:47:33 -0400

    Components added

---
+++ components.js
@@ -0,0 +1,19 @@
+App.RadioButtonsComponent = Ember.Component.extend({
+  classNameBindings: ['style:buttons-radios:radios'],
+});
+
+App.RadioButtonComponent = Em.Component.extend({
+  tagName: 'input',
+  type: 'radio',
+  attributeBindings: ['type', 'value', 'checked'],
+  classNames: ['radio'],
+
+  checked: function() {
+    return this.get('value') === this.get('selection');
+  }.property('selection'),
+
+  click: function(event) {
+    var val = $(this).val();
+    this.set('selection', val);
+  },
+});
